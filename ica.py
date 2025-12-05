44300fc130412ac0318ed550bf95c3f66a8ab8f6
Totrakool Khongsap <totrakool.k@thaibev.com>
2025-12-05 17:30:56 +0700

    Add ica.py file

---
+++ ica.py
@@ -0,0 +1,12 @@
+import numpy as np
+import numpy.linalg as la
+from scipy import signal
+
+def center(X, divide_sd=True):
+    pass
+
+def whiten(X):
+    pass
+
+def ica(X, cycles, tol):
+    pass
